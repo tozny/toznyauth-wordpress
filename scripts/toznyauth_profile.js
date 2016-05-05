e9e7a3cb43302678f4341eec316d7d72bcf9a462
Eric Mann <eric@tozny.com>
2016-05-05 13:32:31 -0700

    Clean up jQuery bindings for max compatibility and performance
    The `jQuery( '#selector' )` pattern is known to be non-performant. A better alternative is to use native DOM methods for retriving an element by ID and pass that element itself into jQuery. This change has been made throughout.
    
    Also, some older browsers have issues with `console.log` calls when the debugging console is not open. To avoid crashes and potential conflicts, we prefix this with a simple `'console' in window` test that, if false, will avoid running out debug log.

--- scripts/toznyauth_profile.js
+++ scripts/toznyauth_profile.js
@@ -1,34 +1,34 @@
 
-jQuery(document).ready(function() {
-    jQuery('#tozny_activate').on('click', function () {
+jQuery( document ).ready(function() {
+    jQuery( document.getelementById( 'tozny_activate' ) ).on( 'click', function () {
             var request_data = {
                 'action': 'create_tozny_user'
             };
-            jQuery.ajax({
+            jQuery.ajax( {
                 url: ajax_object.ajax_url,
                 data: request_data,
                 dataType: 'json',
                 success: function (response,_textStatus,_jqXHR) {
-                    console.log(response);
+                    'console' in window && console.log(response);
                     if (response['success']) {
-                        var template = jQuery('#device_setup_template').html();
+                        var template = jQuery( document.getElementById( 'device_setup_template' ) ).html();
                         var data = response['data'];
                         for (var field in data) {
                             if (data.hasOwnProperty(field)) {
                                 template = template.replace(new RegExp('\\{\\{' + field + '\\}\\}', 'g'), data[field]);
                             }
                         }
-                        jQuery('#enrollment_qr').empty().append(template);
+                        jQuery( document.getElementById( 'enrollment_qr' ) ).empty().append(template);
                         tb_show('TOZNY: Your phone is the key.', '#TB_inline?=true&height=540&width=590&inlineId=enrollment_qr');
                     } else {
                         alert('Could not create a new Tozny user.');
                     }
                 },
                 error: function (_jqXHR,textStatus, errorThrown ) {
-                    console.log('Could not create new Tozny user: '+ textStatus + " -- " +errorThrown);
+                    'console' in window && console.log('Could not create new Tozny user: '+ textStatus + " -- " +errorThrown);
                     alert('Could not complete request to create a new Tozny user.');
                 }
-            });
+            } );
             return false;
-    });
-});
+    } );
+} );
